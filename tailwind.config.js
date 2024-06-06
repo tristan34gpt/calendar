113ecf41eed7cd225c9f23e41d804de13e4d66b9
Tristan vera <tristan.vera19@gmail.com>
2024-06-06 18:34:06 +0200

    add colors for files tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,9 @@ module.exports = {
       backgroundImage: {
         "gradiant-color": "linear-gradient(180deg, #FB8B24 0%, #E95793 100%)",
       },
+      colors: {
+        "gradient-transparent": "rgba(217, 217, 217, 0.49)",
+      },
     },
   },
   plugins: [],
