1eb415b8eb3675f16da34ac8bd9e544e936dc02b
Tristan vera <tristan.vera19@gmail.com>
2024-06-18 11:35:24 +0200

    add loading for createTime

--- src/app/Authentifier/create/vueCalendar/page.jsx
+++ src/app/Authentifier/create/vueCalendar/page.jsx
@@ -12,6 +12,7 @@ export default function View() {
 
   //Variable
   const [view, setView] = useState("");
+  const [loading, setLoading] = useState(false);
 
   const router = useRouter();
 
@@ -32,16 +33,21 @@ export default function View() {
     setView(newView);
   };
 
-  const createView = async () => {
+  const createView = async (e) => {
+    e.preventDefault();
+    setLoading(true);
     if (!view) {
+      setLoading(false);
       return toast.error("Choisissez une vue");
     }
     try {
       await ViewCalendar(view);
       toast.success("Votre visibilité a été enregistrée");
       router.push("/authentifier/create/time");
+      setLoading(false);
     } catch (e) {
       toast.error(e.message);
+      setLoading(false);
     }
   };
   return (
@@ -49,7 +55,7 @@ export default function View() {
       <h1 className="text-[1.4em] font-semibold mb-[80px]">
         Planning visible sur
       </h1>
-      <form action={createView}>
+      <form onSubmit={createView}>
         <div className="flex flex-col">
           {["1 semaine", "2 semaines", "3 semaines", "1 mois"].map(
             (newView, index) => (
@@ -70,9 +76,36 @@ export default function View() {
             )
           )}
         </div>
-        <Button className={"w-[500px] h-[40px] mt-[30px] rounded-md"}>
-          Valider
-        </Button>
+        {!loading ? (
+          <Button
+            type={"submit"}
+            className={"w-[500px] h-[40px] mt-[30px] rounded-md"}
+          >
+            Valider
+          </Button>
+        ) : (
+          <div className="flex justify-center items-center mt-[40px]">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="1.2em"
+              height="1.2em"
+              viewBox="0 0 24 24"
+            >
+              <path
+                fill="currentColor"
+                d="M12,4a8,8,0,0,1,7.89,6.7A1.53,1.53,0,0,0,21.38,12h0a1.5,1.5,0,0,0,1.48-1.75,11,11,0,0,0-21.72,0A1.5,1.5,0,0,0,2.62,12h0a1.53,1.53,0,0,0,1.49-1.3A8,8,0,0,1,12,4Z"
+              >
+                <animateTransform
+                  attributeName="transform"
+                  dur="0.75s"
+                  repeatCount="indefinite"
+                  type="rotate"
+                  values="0 12 12;360 12 12"
+                ></animateTransform>
+              </path>
+            </svg>
+          </div>
+        )}
       </form>
     </div>
   );
