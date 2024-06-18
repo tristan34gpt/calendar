f81c83e8745c5182e1038c4725a285b660a2443a
Tristan vera <tristan.vera19@gmail.com>
2024-06-18 11:39:58 +0200

    add loading for createTime

--- src/app/Authentifier/create/time/page.jsx
+++ src/app/Authentifier/create/time/page.jsx
@@ -14,6 +14,7 @@ export default function Time() {
   const [time, setTime] = useState();
   const [myTime, setMyTime] = useState(null);
   const [modify, setModify] = useState(false);
+  const [loading, setLoading] = useState(false);
   const timeRef = useRef(null);
 
   const router = useRouter();
@@ -32,16 +33,21 @@ export default function Time() {
 
   //Function
 
-  const createTime = async () => {
+  const createTime = async (e) => {
+    e.preventDefault();
+    setLoading(true);
     if (!time) {
+      setLoading(false);
       return toast.error("Choisissez une durée");
     }
 
     try {
       await timeCalendar(time);
       toast.success("Durée de l'appel enregistrez");
-      router.push("/create/questions");
+      router.push("/authentifier/create/questions");
+      setLoading(false);
     } catch (e) {
+      setLoading(false);
       return toast.error(e.message);
     }
   };
@@ -74,7 +80,7 @@ export default function Time() {
       )}
       {(modify || !myTime) && (
         <form
-          action={createTime}
+          onSubmit={createTime}
           className="flex flex-col justify-center items-center"
         >
           <div>
@@ -85,9 +91,36 @@ export default function Time() {
               onChange={() => setTime(timeRef.current.value)}
             />
           </div>
-          <Button className="w-[400px] h-[40px] rounded-md mt-[30px]">
-            Valider
-          </Button>
+          {!loading ? (
+            <Button
+              type={"submit"}
+              className="w-[400px] h-[40px] rounded-md mt-[30px]"
+            >
+              Valider
+            </Button>
+          ) : (
+            <div className="flex justify-center items-center mt-[40px]">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="1.2em"
+                height="1.2em"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  fill="currentColor"
+                  d="M12,4a8,8,0,0,1,7.89,6.7A1.53,1.53,0,0,0,21.38,12h0a1.5,1.5,0,0,0,1.48-1.75,11,11,0,0,0-21.72,0A1.5,1.5,0,0,0,2.62,12h0a1.53,1.53,0,0,0,1.49-1.3A8,8,0,0,1,12,4Z"
+                >
+                  <animateTransform
+                    attributeName="transform"
+                    dur="0.75s"
+                    repeatCount="indefinite"
+                    type="rotate"
+                    values="0 12 12;360 12 12"
+                  ></animateTransform>
+                </path>
+              </svg>
+            </div>
+          )}
         </form>
       )}
     </div>
